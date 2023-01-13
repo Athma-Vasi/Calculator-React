85698d3befe71b4e947d280b2033098be7c28796
Athma Vasi <athma.vasi@protonmail.com>
2023-01-13 14:40:22 -0700

    test button clicks present in display element; add operand local state in calculator component

--- cypress/e2e/app_spec.cy.ts
+++ cypress/e2e/app_spec.cy.ts
@@ -24,3 +24,52 @@ describe("Theme Switching", () => {
     themeSwitch.should("have.class", "left-0");
   });
 });
+
+describe("Display Numbers", () => {
+  it("should display numbers when clicked", () => {
+    cy.visit("http://localhost:3000/");
+
+    //bttn 7 displays 7
+    cy.get("[data-cy='bttn-7']").click();
+    cy.get("[data-cy='display']").should("contain.text", "7");
+
+    //bttn 8 displays 8
+    cy.get("[data-cy='bttn-8']").click();
+    cy.get("[data-cy='display']").should("contain.text", "8");
+
+    //bttn 9 displays 9
+    cy.get("[data-cy='bttn-9']").click();
+    cy.get("[data-cy='display']").should("contain.text", "9");
+
+    //bttn 4 displays 4
+    cy.get("[data-cy='bttn-4']").click();
+    cy.get("[data-cy='display']").should("contain.text", "4");
+
+    //bttn 5 displays 5
+    cy.get("[data-cy='bttn-5']").click();
+    cy.get("[data-cy='display']").should("contain.text", "5");
+
+    //bttn 6 displays 6
+    cy.get("[data-cy='bttn-6']").click();
+    cy.get("[data-cy='display']").should("contain.text", "6");
+
+    //bttn 1 displays 1
+    cy.get("[data-cy='bttn-1']").click();
+    cy.get("[data-cy='display']").should("contain.text", "1");
+
+    //bttn 2 displays 2
+    cy.get("[data-cy='bttn-2']").click();
+    cy.get("[data-cy='display']").should("contain.text", "2");
+
+    //bttn 3 displays 3
+    cy.get("[data-cy='bttn-3']").click();
+    cy.get("[data-cy='display']").should("contain.text", "3");
+
+    //bttn 0 displays 0
+    cy.get("[data-cy='bttn-0']").click();
+    cy.get("[data-cy='display']").should("contain.text", "0");
+
+    //the above sequence of clicks should result in...
+    cy.get("[data-cy='display']").should("contain.text", "7894561230");
+  });
+});
--- src/components/calculator/index.tsx
+++ src/components/calculator/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BttnsContainer } from "../../styledTwComponents/bttnsContainer";
 import { Display } from "../../styledTwComponents/display";
 import { EnterBttn } from "../../styledTwComponents/enterBttn";
@@ -14,44 +14,125 @@ type CalculatorProps = {
 };
 
 function Calculator({ state, action, dispatch }: CalculatorProps) {
+  const [operand, setOperand] = useState("");
+
+  function handleOperandBttnClick(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    const number = event.currentTarget.value;
+    setOperand((prev) => `${prev}${number}`);
+  }
+
   return (
     <div className="grid h-full w-full grid-rows-6 gap-y-5 p-6">
       {/* history */}
       <History state={state}>History</History>
 
       {/* display */}
-      <Display state={state}>Display</Display>
+      <Display state={state} data-cy="display">
+        {operand}
+      </Display>
 
       {/* buttons */}
       <BttnsContainer state={state}>
         {/* row 1 */}
         <div className="row-span-1 grid grid-cols-4 gap-4">
-          <OperandBttn state={state}>7</OperandBttn>
-          <OperandBttn state={state}>8</OperandBttn>
-          <OperandBttn state={state}>9</OperandBttn>
+          <OperandBttn
+            data-cy="bttn-7"
+            state={state}
+            value="7"
+            onClick={handleOperandBttnClick}
+          >
+            7
+          </OperandBttn>
+          <OperandBttn
+            data-cy="bttn-8"
+            state={state}
+            value="8"
+            onClick={handleOperandBttnClick}
+          >
+            8
+          </OperandBttn>
+          <OperandBttn
+            data-cy="bttn-9"
+            state={state}
+            value="9"
+            onClick={handleOperandBttnClick}
+          >
+            9
+          </OperandBttn>
           <OperatorBttn state={state}>DEL</OperatorBttn>
         </div>
 
         {/* row 2 */}
         <div className="row-span-1 grid grid-cols-4 gap-4">
-          <OperandBttn state={state}>4</OperandBttn>
-          <OperandBttn state={state}>5</OperandBttn>
-          <OperandBttn state={state}>6</OperandBttn>
+          <OperandBttn
+            state={state}
+            data-cy="bttn-4"
+            value="4"
+            onClick={handleOperandBttnClick}
+          >
+            4
+          </OperandBttn>
+          <OperandBttn
+            state={state}
+            data-cy="bttn-5"
+            value="5"
+            onClick={handleOperandBttnClick}
+          >
+            5
+          </OperandBttn>
+          <OperandBttn
+            state={state}
+            data-cy="bttn-6"
+            value="6"
+            onClick={handleOperandBttnClick}
+          >
+            6
+          </OperandBttn>
           <OperatorBttn state={state}>+</OperatorBttn>
         </div>
 
         {/* row 3 */}
         <div className="row-span-1 grid grid-cols-4 gap-4">
-          <OperandBttn state={state}>1</OperandBttn>
-          <OperandBttn state={state}>2</OperandBttn>
-          <OperandBttn state={state}>3</OperandBttn>
+          <OperandBttn
+            state={state}
+            data-cy="bttn-1"
+            value="1"
+            onClick={handleOperandBttnClick}
+          >
+            1
+          </OperandBttn>
+          <OperandBttn
+            state={state}
+            data-cy="bttn-2"
+            value="2"
+            onClick={handleOperandBttnClick}
+          >
+            2
+          </OperandBttn>
+          <OperandBttn
+            state={state}
+            data-cy="bttn-3"
+            value="3"
+            onClick={handleOperandBttnClick}
+          >
+            3
+          </OperandBttn>
           <OperatorBttn state={state}>-</OperatorBttn>
         </div>
 
         {/* row 4 */}
         <div className="row-span-1 grid grid-cols-4 gap-4">
           <OperandBttn state={state}>.</OperandBttn>
-          <OperandBttn state={state}>0</OperandBttn>
+          <OperandBttn
+            state={state}
+            data-cy="bttn-0"
+            value="0"
+            onClick={handleOperandBttnClick}
+          >
+            0
+          </OperandBttn>
           <OperandBttn state={state}>/</OperandBttn>
           <OperatorBttn state={state}>X</OperatorBttn>
         </div>
